da7e8efaeb3e20eb322aa6346407c3a8c1e3f321
jinwoo park <swat018@gmail.com>
2022-07-09 14:31:46 +0900

    feat : [순수JS 1] 검색폼 2

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,16 +1,32 @@
-import {qs} from "../helpers.js";
+import {on, qs} from "../helpers.js";
 import View from "./View.js";
+
+const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
     constructor() {
+        console.log(tag, "constructor");
+
         super(qs("#search-form-view"));
 
         this.inputElement = qs("[type=text", this.element)
         this.resetElement = qs("[type=reset]", this.element)
         this.showResetButton(false)
+        this.bindEvent();
     }
 
     showResetButton(visible = true) {
         this.resetElement.style.display = visible ? "block" : "none";
+    }
+
+    bindEvent() {
+        on(this.inputElement, "keyup", () => this.handleKeyup());
+    }
+
+    handleKeyup() {
+        console.log(tag, "handleKeyup", this.inputElement.value);
+        const {value} = this.inputElement
+        this.showResetButton(value.length > 0);
+
     }
 }
