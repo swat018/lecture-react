99407133e29dd47169036ab4b8c374b084db72ba
jinwoo park <swat018@gmail.com>
2022-07-18 06:28:30 +0900

    feat : [순수 JS 2] 탭 1

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,15 +1,17 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView, searchResultView, tabView }) {
     console.log(tag,"constructor");
 
     this.store = store;
 
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
+    this.tabView = tabView;
 
     this.subscribeViewEvents();
+    this.render();
   }
 
   subscribeViewEvents() {
@@ -33,10 +35,12 @@ export default class Controller {
 
   render() {
     if (this.store.searchKeyword.length > 0) {
+      this.tabView.hide()
       this.searchResultView.show(this.store.searchResult);
       return;
     }
 
+    this.tabView.show();
     this.searchResultView.hide();
   }
 }
