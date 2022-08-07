f5a639e7e776cb58c071a08515392635c84f3448
jinwoo park <swat018@gmail.com>
2022-08-07 18:22:32 +0900

    feat : [검색폼 1] Input 요소 사용하기

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,7 +1,15 @@
 const element = (
-    <header>
-        <h2 className="container">검색</h2>
-    </header>
+    <>
+        <header>
+            <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+            <form>
+                <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
+                <button type="reset" className="btn-reset"></button>
+            </form>
+        </div>
+    </>
 );
 
 ReactDOM.render(element, document.querySelector("#app"))
