747b7f1b9956fe0ca1f901b15f7d875df8af8ffa
jinwoo park <swat018@gmail.com>
2022-08-08 06:55:32 +0900

    feat : [검색폼 1] 컴포넌트

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,15 +1,19 @@
-const element = (
-    <>
-        <header>
-            <h2 className="container">검색</h2>
-        </header>
-        <div className="container">
-            <form>
-                <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
-                <button type="reset" className="btn-reset"></button>
-            </form>
-        </div>
-    </>
-);
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <header>
+                    <h2 className="container">검색</h2>
+                </header>
+                <div className="container">
+                    <form>
+                        <input type="text" placeholder="검색어를 입력하세요" autoFocus/>
+                        <button type="reset" className="btn-reset"></button>
+                    </form>
+                </div>
+            </>
+        )
+    }
+}
 
-ReactDOM.render(element, document.querySelector("#app"))
+ReactDOM.render(<App />, document.querySelector("#app"))
