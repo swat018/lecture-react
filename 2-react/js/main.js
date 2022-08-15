b2e5db5714b886d57351541e2747189ff8b448ce
jinwoo park <swat018@gmail.com>
2022-08-15 14:50:49 +0900

    feat : [검색 결과] 리스트와 키

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -7,6 +7,7 @@ class App extends React.Component {
         this.state = {
             searchKeyword: "",
             searchResult: [],
+            submitted: false,
         }
     }
 
@@ -27,7 +28,7 @@ class App extends React.Component {
 
     search(searchKeyword) {
         const searchResult = store.search(searchKeyword);
-        this.setState({searchResult});
+        this.setState({searchResult, submitted: true});
     }
 
     handleReset() {
@@ -40,43 +41,49 @@ class App extends React.Component {
     }
 
     render() {
+        const searchForm = (
+            <form
+                onSubmit={event => this.handleSubmit(event)}
+                onReset={() => this.handleReset()}
+            >
+                <input
+                    type="text"
+                    placeholder="검색어를 입력하세요"
+                    autoFocus
+                    value={this.state.searchKeyword}
+                    onChange={event => this.handleChangeInput(event)}
+                />
+                {this.state.searchKeyword.length > 0 && (
+                    <button type="reset" className="btn-reset"></button>
+                )}
+            </form>
+        );
+
+        const searchResult = (
+            this.state.searchResult.length > 0 ? (
+                <ul className="result">
+                    {this.state.searchResult.map((item, index) => {
+                        return (
+                            <li key={item.id}>
+                                <img src={item.imageUrl} alt={item.name}/>
+                                <p>{item.name}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            ) : (
+                <div className="empty-box">검색 결과가 없습니다.</div>
+            )
+        );
+
         return (
             <>
                 <header>
                     <h2 className="container">검색</h2>
                 </header>
                 <div className="container">
-                    <form
-                        onSubmit={event => this.handleSubmit(event)}
-                        onReset={() => this.handleReset()}
-                    >
-                        <input
-                            type="text"
-                            placeholder="검색어를 입력하세요"
-                            autoFocus
-                            value={this.state.searchKeyword}
-                            onChange={event => this.handleChangeInput(event)}
-                        />
-                        {this.state.searchKeyword.length > 0 && (
-                            <button type="reset" className="btn-reset"></button>
-                        )}
-                    </form>
-                    <div className="content">
-                        {this.state.searchResult.length > 0 ? (
-                            <ul className="result">
-                                {this.state.searchResult.map(item => {
-                                    return (
-                                        <li>
-                                            <img src={item.imageUrl} alt={item.name}/>
-                                            <p>{item.name}</p>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        ) : (
-                            <div className="empty-box">검색 결과가 없습니다.</div>
-                        )}
-                    </div>
+                    {searchForm}
+                    <div className="content">{this.state.submitted && searchResult}</div>
                 </div>
             </>
         )
