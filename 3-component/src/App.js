3fe2691a5afcd2c4b3991cc0a29a0d9004ee6860
jinwoo park <swat018@gmail.com>
2022-08-27 21:14:59 +0900

    feat : [소개] Header: 함수 컴포넌트

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
+import Header from "./components/Header.js";
 
 export default class App extends React.Component {
     render() {
-        return <>TODO: App 컴포넌트</>;
+        return <Header />;
     }
 }
